bde2c46e42af8dbcab8b2103919126c6bd43df82
christyboateng32-tech <christyboateng32@gmail.com>
2026-05-15 01:16:58 +0200

    I added some stuff in the database ! Tadaa i am done!

--- ChapeauProject/ChapeauProject/Repositories/MenuRepository.cs
+++ ChapeauProject/ChapeauProject/Repositories/MenuRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.Data;
 
 namespace ChapeauProject.Repositories
 {
@@ -17,44 +18,54 @@ namespace ChapeauProject.Repositories
         public List<MenuItem> GetFiltered(string cardFilter, string courseFilter)
         {
             List<MenuItem> items = new List<MenuItem>();
+
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
-                // We use LEFT JOIN to ensure items show up even if Stock or Course data is missing
-                string query = @"SELECT MI.MenuItemID, MI.ItemName, MI.Price, 
-                                       ISNULL(S.Quantity, 0) AS Quantity, 
-                                       ISNULL(C.CourseName, 'N/A') AS CourseName, 
-                                       MI.MenuCard 
-                                FROM MenuItems MI
-                                LEFT JOIN Stock S ON MI.MenuItemID = S.MenuItemID
-                                LEFT JOIN Courses C ON MI.CourseID = C.CourseID
-                                WHERE MI.MenuCard = @MenuCard";
+                // LEFT JOIN ensures items appear even if Stock or Course links are missing [cite: 7, 13, 19]
+                // UPPER() handles potential case-sensitivity issues between C# and SQL
+                string query = @"
+                    SELECT 
+                        MI.MenuItemID, 
+                        MI.ItemName, 
+                        MI.Price, 
+                        ISNULL(S.Quantity, 0) AS Quantity, 
+                        ISNULL(C.CourseName, 'N/A') AS CourseName, 
+                        MI.MenuCard 
+                    FROM MenuItems MI
+                    LEFT JOIN Stock S ON MI.MenuItemID = S.MenuItemID
+                    LEFT JOIN Courses C ON MI.CourseID = C.CourseID
+                    WHERE UPPER(MI.MenuCard) = UPPER(@MenuCard)";
 
                 if (courseFilter != "All")
                 {
-                    query += " AND C.CourseName = @CourseName";
+                    query += " AND UPPER(C.CourseName) = UPPER(@CourseName)";
                 }
 
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@MenuCard", cardFilter);
+
                     if (courseFilter != "All")
                     {
                         command.Parameters.AddWithValue("@CourseName", courseFilter);
                     }
 
                     connection.Open();
+
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            items.Add(new MenuItem(
+                            // Mapping database columns to the MenuItem model
+                            MenuItem item = new MenuItem(
                                 (int)reader["MenuItemID"],
                                 (string)reader["ItemName"],
                                 (decimal)reader["Price"],
                                 (int)reader["Quantity"],
                                 (string)reader["CourseName"],
                                 (string)reader["MenuCard"]
-                            ));
+                            );
+                            items.Add(item);
                         }
                     }
                 }
--- ChapeauProject/ChapeauProject/obj/Debug/net8.0/ChapeauProject.AssemblyInfo.cs
+++ ChapeauProject/ChapeauProject/obj/Debug/net8.0/ChapeauProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ed667ba51317dc0eccc6f8f50523d26e95bfd1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f26ec97e349aad49e36de83a0d54af8c0d6c990c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
