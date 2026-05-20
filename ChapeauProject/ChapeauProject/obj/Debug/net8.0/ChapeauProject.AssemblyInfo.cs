915251e3d8c58a86b6244171d772487653fdb6f9
christyboateng32-tech <christyboateng32@gmail.com>
2026-05-20 21:40:41 +0200

    stages

--- ChapeauProject/ChapeauProject/obj/Debug/net8.0/ChapeauProject.AssemblyInfo.cs
+++ ChapeauProject/ChapeauProject/obj/Debug/net8.0/ChapeauProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe56970240997329bce87b4e2d8550630e65f3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1cba0f405a87864548851dfd0d42f995b778d6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
