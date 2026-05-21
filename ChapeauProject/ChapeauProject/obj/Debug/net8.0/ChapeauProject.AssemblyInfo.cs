627f7c0d17224449d188ebae836a668666ceb760
LucaMulders <746004@student.inholland.nl>
2026-05-21 12:34:22 +0200

    Changes

--- ChapeauProject/ChapeauProject/obj/Debug/net8.0/ChapeauProject.AssemblyInfo.cs
+++ ChapeauProject/ChapeauProject/obj/Debug/net8.0/ChapeauProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b77018b0da6438d70ddced9c3b44d47aa58173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e23966efc94a700dcd1871790183e679443d4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChapeauProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
