c5f869367b2a2ec09296d3e709d591388f4e5ec8
L_Void_X <47220606+LVoidX@users.noreply.github.com>
2026-05-21 02:03:11 +0200

    Changes finally done :,)

---
+++ ChapeauProject/ChapeauProject/Controllers/OrdersController.cs
@@ -0,0 +1,35 @@
+﻿using ChapeauProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapeauProject.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IActionResult Index()
+        {
+            var orders = _orderService.GetRunningOrders();
+            return View(orders);
+        }
+
+        [HttpPost]
+        public IActionResult ToggleItem(int orderItemId)
+        {
+            _orderService.ToggleItemPreparation(orderItemId);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult CompleteOrder(int orderId)
+        {
+            _orderService.CompleteOrder(orderId);
+            return RedirectToAction("Index");
+        }
+    }
+}
